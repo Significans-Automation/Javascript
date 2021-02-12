ef52506e701b335946ed34cc725824bd1a0aeac4
sigdavis <78979333+sigdavis@users.noreply.github.com>
2021-02-12 16:09:33 -0500

    Update Round To Sixteenth.js

--- General/Round To Sixteenth.js
+++ General/Round To Sixteenth.js
@@ -1,5 +1,11 @@
-function() f {
-	alert("");
+function round(n, denom=16, r="higher") {
+	
+	var result;
+	if (r == "higher") {
+		result = Math.ceil(n * denom) / denom;
+  	} else {
+		result = Math.floor(n * denom) / denom;
+  	}
+  	return result;
+	
 }
-
-f();
